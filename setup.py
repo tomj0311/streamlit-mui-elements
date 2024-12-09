fe75cf43cc898dcf80a50f560c8013d95e65162d
Tom Jose <tomjose@zealogics.com>
2024-12-09 10:30:30 +0400

    bump: update version to 0.0.2 in setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="streamlit-mui-elements",
-    version="0.0.1",
+    version="0.0.2",
     author="Tom Jose",
     author_email="tomj0311@gmail.com",
     description="",
