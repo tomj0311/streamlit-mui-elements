68a8376fad8d8fe883ec7799f0ee392b3bbd75e3
Tom Jose <tomjose@zealogics.com>
2024-12-18 12:22:45 +0400

    feat: update version to 0.0.10 in setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="streamlit-mui-elements",
-    version="0.1.0",
+    version="0.0.10",
     author="Tom Jose",
     author_email="tomj0311@gmail.com",
     description="A Streamlit component for Material-UI elements",
